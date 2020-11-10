a1f84aceba6b1368f8e42583b56349b719393818
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-10 19:10:39 -0400

    Esboço para menu responsivo

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
         Implementar via JS sistema de stickNavigation!
     */
 
+    $('#click').click(function() {
+
+        $('.aside').animate({width:'toggle'}, 1500)
+
+    })
+
+
+
 })
