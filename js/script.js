8d7d44998762cc3ead2fa20579cbbbaaadee7635
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-11 13:05:00 -0400

    header, header navigation e menu

--- js/script.js
+++ js/script.js
@@ -2,11 +2,25 @@ $(document).ready(function () {
 
     /*
         Implementar via JS sistema de stickNavigation!
+        botar seletores em variáveis e sistema de desmarcação no body
     */
 
-    $('#click').click(function() {
+    $('#check').change(function() {
 
-        $('.aside').animate({width:'toggle'}, 1500)
+        var checked = $('#check').is(':checked');
+        console.log(checked);
+
+        if(checked){
+            $('.abrir').delay('slow').fadeIn();
+            $('.fechar').delay().fadeOut('slow');
+        }else{
+            $('.abrir').delay().fadeOut('slow');
+            $('.fechar').delay('slow').fadeIn();
+        }       
+
+        $('.aside').animate({width:'toggle'}, 'slow');        
+
+
 
     })
 
